e2fb9f738b09f7e83b3c239db387b4773261dbe2
Likhith Kolayari <likhith@regentmarkets.com>
2022-01-26 13:19:17 +0400

    Implemented QR code generator

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import style from './App.module.scss';
+import QrCode from './components/qr-code/qr-code';
 
 function App() {
   return (
     <div className={style.app}>
        <div className={style.start}>start</div>
+       <QrCode/>
     </div>
   );
 }
